7bfa0469499e04ad758a32fcb0ca2a300484c708
nikunj <nikunjsinghal09032004@gmail.com>
2024-03-16 17:09:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelector('sections');
+let navLinks = document.querySelector('header nav a');
+
+window.onscroll = () => {
+    sections.foreach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset & top < offset  + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ']'),classList.add{'active'};
+            })
+        }
+    })
+}
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
